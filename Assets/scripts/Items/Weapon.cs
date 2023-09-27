371b913f660b785e3f5b54025622e2095c8f7b2d
1BrainStorm9 <nikolaenko031@gmail.com>
2023-09-27 16:12:00 +0300

    test

--- Assets/scripts/Items/Weapon.cs
+++ Assets/scripts/Items/Weapon.cs
@@ -6,7 +6,4 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] public AssetItem asset;
     [SerializeField] public List<Spell> spells;
-    [SerializeField] public AssetItem A;
-    [SerializeField] public List<Spell> B;
-
 }
