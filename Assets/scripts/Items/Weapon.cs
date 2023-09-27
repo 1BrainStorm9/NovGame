bd40c831e4dba6abba200618cde05b0b752a11b2
1BrainStorm9 <nikolaenko031@gmail.com>
2023-09-27 16:22:16 +0300

    Привел всё в норму

--- Assets/scripts/Items/Weapon.cs
+++ Assets/scripts/Items/Weapon.cs
@@ -6,5 +6,4 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] public AssetItem asset;
     [SerializeField] public List<Spell> spells;
-    [SerializeField] public List<Spell> spells2;
 }
